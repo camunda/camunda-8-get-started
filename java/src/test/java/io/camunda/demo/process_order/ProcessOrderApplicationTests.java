3850184387bb15ef57558474542ee63bd34e5b31
Josh Wulf <josh.wulf@camunda.com>
2025-07-08 15:06:18 +1200

    start CPT integration

--- java/src/test/java/io/camunda/demo/process_order/ProcessOrderApplicationTests.java
+++ java/src/test/java/io/camunda/demo/process_order/ProcessOrderApplicationTests.java
@@ -1,13 +1,43 @@
 package io.camunda.demo.process_order;
 
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.response.ProcessInstanceEvent;
+import io.camunda.process.test.api.CamundaAssert;
+import io.camunda.process.test.api.CamundaProcessTestContext;
+import io.camunda.process.test.api.CamundaSpringProcessTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProcessOrderApplicationTests {
+@CamundaSpringProcessTest
+public class ProcessOrderApplicationTests {
+    @Autowired
+    private CamundaClient client;
+    @Autowired
+    private CamundaProcessTestContext processTestContext;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void shouldCompleteProcessInstance() {
+        // given: the processes are deployed
+        client
+                .newDeployResourceCommand()
+                .addResourceFromClasspath("diagram_1.bpmn")
+                .send()
+                .join();
 
+        // when
+        final ProcessInstanceEvent processInstance = client
+                .newCreateInstanceCommand()
+                .bpmnProcessId("process1")
+                .latestVersion()
+                .send()
+                .join();
+        processTestContext.mockJobWorker("check-inventory").thenComplete();
+        processTestContext.mockJobWorker("charge-payment").thenComplete();
+        processTestContext.mockJobWorker("ship-items").thenComplete();
+        // then
+        CamundaAssert.assertThat(processInstance).isCompleted();
+    }
 }
+
